5c01838f50dcbe1ca468da17b2f068b16b5d4fb1
Anni Voore <anni.voore@voco.ee>
2024-12-11 13:58:16 +0200

    typical-page projekti loomine

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+import MainHeader from './components/MainHeader/MainHeader';
+import Login from './components/Login/Login';
+
+function App() {
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>
+        <Login />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
